1e43c3a4e3054290ec8d30ce0e8ec58f301fbacb
Damon Vinciguerra <damon.vinciguerra@cse-icon.com>
2025-05-12 17:10:38 -0600

    cross your fingers

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ def count_weekly_workouts():
     return jsonify(count=count)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(port=int(os.environ.get("PORT", 8080)))
